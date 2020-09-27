ad804f8cb26dbe29e6b5c5f6ed5fbcfaaa76d7ab
pjhubs <877302410@qq.com>
2020-09-27 09:52:49 +0800

    2D - 熟悉人物移动、翻转

--- 2DLearning/Assets/Scripts/Player/PlayerController.cs
+++ 2DLearning/Assets/Scripts/Player/PlayerController.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+
+    public float speed;
+    public float jumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+
+        if (horizontalInput != 0)
+        {
+            transform.localScale = new Vector3(horizontalInput, 1, 1);
+        }
     }
 }
