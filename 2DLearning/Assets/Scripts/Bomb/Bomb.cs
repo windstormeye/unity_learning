6925ec94151a09c5c5c27e832fe8d6590ade2906
pjhubs <877302410@qq.com>
2020-11-11 22:35:36 +0800

    2D - 炸弹爆炸效果

--- 2DLearning/Assets/Scripts/Bomb/Bomb.cs
+++ 2DLearning/Assets/Scripts/Bomb/Bomb.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+
+    private Animator anim;
+    private Collider2D coll;
+    private Rigidbody2D rb;
+
+    public float startTime;
+    public float waitTime;
+    public float bombForce;
+
+    [Header("Check")]
+    public float radius;
+    public LayerMask targetLayer;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > startTime + waitTime)
+        {
+            anim.Play("bomb_explotion");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public void Explotion()
+    {
+        coll.enabled = false;
+        rb.gravityScale = 0;
+        // NOTE: 遍历当前炸弹 radius 范围内的 targetLayer 上的物体
+        Collider2D[] aroundObjects = Physics2D.OverlapCircleAll(transform.position, radius, targetLayer);
+        foreach (var item in aroundObjects)
+        {
+            Vector3 pos = transform.position - item.transform.position;
+            item.GetComponent<Rigidbody2D>().AddForce((-pos + Vector3.up) * bombForce, ForceMode2D.Impulse);
+
+        }
+    }
+
+    public void DestroyThis()
+    {
+        Destroy(gameObject);
     }
 }
