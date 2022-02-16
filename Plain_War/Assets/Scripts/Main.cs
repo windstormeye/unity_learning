f6ce75eb4c887b83c4de510e7f2091a1e7e6f9e9
windstormeye <877302410@qq.com>
2022-02-16 13:16:34 +0800

    完成 - 背景滚动

--- Plain_War/Assets/Scripts/Main.cs
+++ Plain_War/Assets/Scripts/Main.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
     public GameObject[] monsterPrefabs;
+    public GameObject bgPrefab;
+    public float bgSpeed = 1.2f;
 
     private float timePass = 0;
     private float timeInterver = 2;
+
+    private GameObject bg0;
+    private GameObject bg1;
+    private GameObject bg2;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
+
+        initBackground();
     }
 
     // Update is called once per frame
@@ -24,6 +33,8 @@ public class Main : MonoBehaviour
             CreateMonster();
             timePass = 0;
         }
+
+        updateBackground();
     }
 
     private void CreateMonster()
@@ -34,5 +45,39 @@ public class Main : MonoBehaviour
         int monsterPrefeb = Random.Range(0, monsterPrefabs.Length);
         GameObject monster = Instantiate(monsterPrefabs[monsterPrefeb]);
         monster.transform.position = new Vector3(monsterX, monsterY, 0);
+    }
+
+    private void initBackground()
+    {
+        bg0 = Instantiate(bgPrefab);
+        bg1 = Instantiate(bgPrefab);
+        bg2 = Instantiate(bgPrefab);
+
+        bg0.transform.Translate(0, 20, 0);
+        bg1.transform.Translate(0, 10, 0);
+        bg2.transform.Translate(0, 0, 0);
+    }
+
+    private void updateBackground()
+    {
+        float step = bgSpeed * Time.deltaTime;
+        bg0.transform.Translate(0, -step, 0);
+        bg1.transform.Translate(0, -step, 0);
+        bg2.transform.Translate(0, -step, 0);
+
+        if (bg0.transform.position.y <= -10)
+        {
+            bg0.transform.Translate(0, 20, 0);
+        }
+
+        if (bg1.transform.position.y <= -10)
+        {
+            bg1.transform.Translate(0, 20, 0);
+        }
+
+        if (bg2.transform.position.y <= -10)
+        {
+            bg2.transform.Translate(0, 20, 0);
+        }
     }
 }
